c5d0a1409342c597171d4d8ce354067f31fd72c8
Aaditya Joshi <aadityajoshi151@gmail.com>
2022-06-12 21:03:38 +0530

    added requirements and configured driver

--- src/Whatsapp-Group-Contacts-Exporter.py
+++ src/Whatsapp-Group-Contacts-Exporter.py
@@ -1,7 +1,10 @@
-#imports
+from selenium import webdriver
 
 def main():
-    print("Main Begins")
+    driver_path = "../WebDriver/chromedriver"
+    driver = webdriver.Chrome(executable_path = driver_path)
+    driver.get("https://web.whatsapp.com/")
+    input("Login to whatsapp web and press enter to continue")
 
 if __name__ == "__main__":
     main()
