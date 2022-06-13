a6dbfb2bba04188eb031d98d13a205ca03f7f010
Aaditya Joshi <aadityajoshi151@gmail.com>
2022-06-13 19:41:17 +0530

    added input and search feature

--- src/Whatsapp-Group-Contacts-Exporter.py
+++ src/Whatsapp-Group-Contacts-Exporter.py
@@ -1,10 +1,16 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 def main():
+    grp_name = input("Enter Group/Chat name\n")
     driver_path = "../WebDriver/chromedriver"
-    driver = webdriver.Chrome(executable_path = driver_path)
+    options = Options()
+    options.add_argument('user-data-dir=./User_Data')
+    driver = webdriver.Chrome(executable_path = driver_path, chrome_options=options)
     driver.get("https://web.whatsapp.com/")
     input("Login to whatsapp web and press enter to continue")
+    driver.find_element_by_xpath('/html/body/div[1]/div/div/div[3]/div/div[1]/div/div/div[2]/div/div[2]').send_keys(grp_name)
+    input()
 
 if __name__ == "__main__":
     main()
