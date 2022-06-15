0017aa132d7b12acf7ed7d141c82236e55ffec64
Aaditya Joshi <aadityajoshi151@gmail.com>
2022-06-15 18:31:22 +0530

    Select the group name from search results

--- src/Whatsapp-Group-Contacts-Exporter.py
+++ src/Whatsapp-Group-Contacts-Exporter.py
@@ -2,14 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 def main():
-    grp_name = input("Enter Group/Chat name\n")
+    grp_name = input("Enter group name\n")
     driver_path = "../WebDriver/chromedriver"
     options = Options()
     options.add_argument('user-data-dir=./User_Data')
     driver = webdriver.Chrome(executable_path = driver_path, chrome_options=options)
     driver.get("https://web.whatsapp.com/")
-    input("Login to whatsapp web and press enter to continue")
+    input("Scan the QR code to login to whatsapp web and press enter on sucessful login\n")
     driver.find_element_by_xpath('/html/body/div[1]/div/div/div[3]/div/div[1]/div/div/div[2]/div/div[2]').send_keys(grp_name)
+    driver.find_element_by_xpath("//span[@title='"+grp_name+"']").click()
     input()
 
 if __name__ == "__main__":
