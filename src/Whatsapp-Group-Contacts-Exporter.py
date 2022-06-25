19c087c0168ebfe82d8110b2cb196c22bfac0cb1
Aaditya Joshi <aadityajoshi151@gmail.com>
2022-06-25 11:49:39 +0530

    get one name from list nodes

--- src/Whatsapp-Group-Contacts-Exporter.py
+++ src/Whatsapp-Group-Contacts-Exporter.py
@@ -2,18 +2,32 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 def main():
-    grp_name = input("Enter group name\n")
+    #grp_name = input("Enter group name\n")
+    grp_name = "Pictionary"
     driver_path = "../WebDriver/chromedriver"
     options = Options()
     options.add_argument('user-data-dir=./User_Data')
     driver = webdriver.Chrome(executable_path = driver_path, chrome_options=options)
     driver.get("https://web.whatsapp.com/")
-    input("Scan the QR code to login to whatsapp web and press enter on sucessfull login\n")
+    input("Scan the QR code to login to whatsapp web and press enter on sucessful login\n")
     driver.find_element_by_xpath('/html/body/div[1]/div/div/div[3]/div/div[1]/div/div/div[2]/div/div[2]').send_keys(grp_name)
     driver.find_element_by_xpath("//span[@title='"+grp_name+"']").click()
     driver.find_element_by_xpath('/html/body/div[1]/div/div/div[4]/div/header/div[2]').click()
     driver.find_element_by_xpath('/html/body/div[1]/div/div/div[2]/div[3]/span/div/span/div/div/section/div[6]/div[1]/div/div/div[1]').click()
+    div_nodes = driver.find_elements_by_css_selector('div.KPJpj:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)')
+
+    print(f"Length: {len(div_nodes)}")
+    # for i in range(len(div_nodes)):
+    #     print(div_nodes[i].find_element_by_xpath('//div/div/div[2]/div/div/span').text)
+    # for i in div_nodes:
+    #     try:
+    #         print(i)
+    #         i.find_element_by_xpath('//div/div/div[2]/div/div/span').text
+    #     except Exception as e:
+    #         print("Not found, moving to next element")
+    #         pass
     input()
+    
 
 if __name__ == "__main__":
     main()
