90ef6087a9584a73d42a0a1ac539feca02d7251d
Aaditya Joshi <aadityajoshi151@gmail.com>
2022-06-26 10:47:56 +0530

    getting partial names

--- src/Whatsapp-Group-Contacts-Exporter.py
+++ src/Whatsapp-Group-Contacts-Exporter.py
@@ -1,9 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
 def main():
-    #grp_name = input("Enter group name\n")
-    grp_name = "Pictionary"
+    grp_name = input("Enter group name\n")
     driver_path = "../WebDriver/chromedriver"
     options = Options()
     options.add_argument('user-data-dir=./User_Data')
@@ -13,19 +13,14 @@ def main():
     driver.find_element_by_xpath('/html/body/div[1]/div/div/div[3]/div/div[1]/div/div/div[2]/div/div[2]').send_keys(grp_name)
     driver.find_element_by_xpath("//span[@title='"+grp_name+"']").click()
     driver.find_element_by_xpath('/html/body/div[1]/div/div/div[4]/div/header/div[2]').click()
-    driver.find_element_by_xpath('/html/body/div[1]/div/div/div[2]/div[3]/span/div/span/div/div/section/div[6]/div[1]/div/div/div[1]').click()
-    div_nodes = driver.find_elements_by_css_selector('div.KPJpj:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)')
-
-    print(f"Length: {len(div_nodes)}")
-    # for i in range(len(div_nodes)):
-    #     print(div_nodes[i].find_element_by_xpath('//div/div/div[2]/div/div/span').text)
-    # for i in div_nodes:
-    #     try:
-    #         print(i)
-    #         i.find_element_by_xpath('//div/div/div[2]/div/div/span').text
-    #     except Exception as e:
-    #         print("Not found, moving to next element")
-    #         pass
+    grp_members_info = driver.find_element_by_xpath('/html/body/div[1]/div/div/div[2]/div[3]/span/div/span/div/div/section/div[6]/div[1]/div/div/div[1]')
+    print(f"Group Members: {grp_members_info.text}")
+    grp_members_info.click()
+    for i in range(2,11):
+        print(i)
+        #print(driver.find_element_by_xpath(f).text)
+        print(driver.find_element(by=By.XPATH, value=f'/html/body/div[1]/div/span[2]/div/span/div/div/div/div/div/div/div[2]/div/div/div/div[{i}]/div/div/div[2]/div[1]/div/span').text)
+        print("-"*50)
     input()
     
 
